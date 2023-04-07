244da197646ac0a8179f5cf4dbea11d1635782da
gaston becerra <gaston.becerra@gmail.com>
2023-04-07 13:11:35 -0300

    poniendo fulltext en csv de alan

--- scopus100oa.R
+++ scopus100oa.R
@@ -1,3 +1,5 @@
+#2do: corregir las rutas para distinguir fulltext y data (productos)
+
 library(tidyverse)
 library(readxl)
 
@@ -8,10 +10,10 @@ glimpse(scopus100)
 
 # import data from CrossRef using DOI -----------------------------
 
-# install.packages("rcrossref")
+# install.packages("rcrossref")git 
 # library('rcrossref')
 # cr100 <- rcrossref::cr_works(doi = scopus100$DOI)
-saveRDS(cr100, file = "./scopus100_cr.RData")
+# saveRDS(cr100, file = "./scopus100_cr.RData")
 
 cr100 <- readRDS("../data/scopus100_cr.RData")
 glimpse(cr100$data)
@@ -32,8 +34,11 @@ links <- cr100$data %>% select(doi, link, language) %>%
 glimpse(links)
 links %>% write.csv("./scopus100_links.csv", row.names = FALSE)
 
+links <- read.csv("./scopus100_links.csv")
+
 # download pdfs
 download_error_log <- c()
+#2do: que pancho... mas que el error, habia que registrar los uqe sí bajaron
 for (i in 1:nrow(links)) {
     tryCatch( {
         download.file(links$URL[i], 
@@ -56,8 +61,28 @@ rm(i)
 # hay otras URL que son de un OJS... se tienen que poder tomar directo
 # hay otras que son de un repositorio de la universidad... se tienen que poder tomar directo
 
+links %>% left_join( cr100$data , by = "doi") %>% glimpse()
+
+# list downloaded files in directory "./fulltext/"
+library(fs)
+downloaded_pdf <- dir_ls("./fulltext/") %>%
+        as.character() %>% as_tibble() %>% 
+        rename(filename = value) %>%
+        mutate(id = str_extract(filename, "[0-9]+")) %>%
+        mutate(id = as.numeric(id)) %>% 
+        select(id, filename) %>%
+        left_join(links, by = "id") %>%
+        glimpse()
+
 # read pdfs -----------------------------
 
+
+# primero tengo que bajar los lenguajes que necesito
+
+table(links$language) 
+table(downloaded_pdf$language)
+
+# https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html
 library(tesseract)
 tesseract_download("spa")
 tesseract_download("fra")
@@ -65,11 +90,49 @@ tss.spa <- tesseract("spa")
 tss.fra <- tesseract("fra")
 tss.eng <- tesseract("eng")
 
-# https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html
 
-table(links$language)
+# read pdfs listed in downloaded_pdf
+pdf_text <- c()
+for (i in 1:nrow(downloaded_pdf)) {
+    tryCatch( {
+        ocr <- tesseract::ocr(
+            downloaded_pdf$filename[i], 
+            tss.eng) %>% paste(collapse = " **pagebreak** ")
+        pdf_text <<- c(pdf_text, ocr) 
+        print(i)
+        },
+    error = function(e) {
+        print(i)
+        print(e)
+        pdf_text <<- c(pdf_text, NA)
+    }
+    )
+    Sys.sleep(1)
+}
+
+downloaded_pdf$text <- pdf_text
+
+sum(is.na(downloaded_pdf$text))
+
+saveRDS(downloaded_pdf, file = "./scopus100_fulltext.RData")
+
+rm(ocr,i,pdf_text)
 
 
+
+glimpse(downloaded_pdf)
+glimpse(scopus100)
+
+# merge data -----------------------------
+scopus100 %>% 
+    left_join(
+        downloaded_pdf %>% 
+        filter(!is.na(text)) %>% 
+        filter(intended.application == "similarity-checking") %>%
+        rename(DOI=doi)
+        , by = "DOI") %>% 
+    select(-c("id", "filename", "URL", "content.type", "language")) %>% 
+    write.csv("./scopus100_fulltext.csv", row.names = FALSE)
 
 
 # referencias -----------------------------
